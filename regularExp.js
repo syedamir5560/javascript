b8bc0c095074c0f7050f5b246c1e7f6333e6dd41
syedamir5560 <syedamir5560@gmail.com>
2023-11-17 15:03:17 +0530

    s

---
+++ regularExp.js
@@ -0,0 +1,10 @@
+/*
+Regular Expression:=>
+
+A regular expression is a sequence of characters that forms a search pattern. The search pattern can be used for text search and text to replace operations. A regular expression can be a single character or a more complicated pattern. Regular expressions can be used to perform all types of text search and text replacement operations.
+In JavaScript, a Regular Expression (RegEx) is an object that describes a sequence of characters used for defining a search pattern. For example,
+
+Syntax:=>
+
+/pattern/modifiers;
+
